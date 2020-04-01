4179e8783fe17707f1ae385ef15b72bf5db7c0f6
DyNamiC <imsong1997@gmail.com>
2020-04-02 00:09:45 +0900

    test

--- MapAPI.js
+++ MapAPI.js
@@ -1,21 +1,11 @@
-API_KEY = "2a7a6e03114918d4754b99f8d6b3894b"
-
 function getMap(){
-
-    fetch(
-        `//dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}`
-    ).then(function(response){
-        return response.json();
-    }).then(function(response){
-        console.log(json);
-    });
-
-    // var container = document.getElementById('map');
-    // var options = {
-    //     center: new kakao.maps.LatLng(33.450701, 126.570667),
-    //     level: 3
-    // };
-    // var map = new kakao.maps.Map(container, options);
+    
+    var container = document.getElementById('map');
+    var options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
+    };
+    var map = new kakao.maps.Map(container, options);
 }
 
 getMap();
