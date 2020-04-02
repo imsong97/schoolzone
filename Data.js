1219854467f1ccfbc48e3994f49184d5d6887f44
DyNamiC <imsong1997@gmail.com>
2020-04-02 22:22:06 +0900

    0402

--- Data.js
+++ Data.js
@@ -1,19 +1,19 @@
-const data = {
-    serviceKey: 'I%2Bu5yBZvAbz%2FgetqfPM1BQ%2FEShTpdA%2FCmaJOCUCSzvtSfbvPtETYt57IJ54u%2FmsQZRRQOLY12xGLJ1R4qHppJg%3D%3D',
-    s_page: 0,
-    s_list: 1,
-    type: 'json'
-};
+// const data = {
+//     serviceKey: 'I%2Bu5yBZvAbz%2FgetqfPM1BQ%2FEShTpdA%2FCmaJOCUCSzvtSfbvPtETYt57IJ54u%2FmsQZRRQOLY12xGLJ1R4qHppJg%3D%3D',
+//     s_page: 0,
+//     s_list: 1,
+//     type: 'json'
+// };
 
-$.ajax({
-    method: "GET",
-    url: 'http://api.data.go.kr/openapi/child-prtc-zn-std',
-    data: data,
-    dataType: 'json',
-    success: function(data){
-         console.log(data);
-    }
-});
+// $.ajax({
+//     method: "GET",
+//     url: 'http://api.data.go.kr/openapi/child-prtc-zn-std',
+//     data: data,
+//     dataType: 'json',
+//     success: function(data){
+//          console.log(data);
+//     }
+// });
 // const API_KEY = "I%2Bu5yBZvAbz%2FgetqfPM1BQ%2FEShTpdA%2FCmaJOCUCSzvtSfbvPtETYt57IJ54u%2FmsQZRRQOLY12xGLJ1R4qHppJg%3D%3D";
 
 // function getData(){
@@ -27,3 +27,6 @@ $.ajax({
 // }
 
 // getData();
+
+const myData = JSON.parse(data)
+console.log(myData[0].pageNo);
