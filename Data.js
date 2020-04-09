19a907f0d5b409acafb0867d6b5d3a7b8cdd4a58
DyNamiC <imsong1997@gmail.com>
2020-04-09 19:55:20 +0900

    Update Data.js

--- Data.js
+++ Data.js
@@ -12,7 +12,7 @@
 
 var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
     center : new kakao.maps.LatLng(36.2683, 127.6358), // 지도의 중심좌표 
-    level : 14 // 지도의 확대 레벨 
+    level : 3 // 지도의 확대 레벨 
 });
 
 // // 마커 클러스터러를 생성합니다 
