1e1d4995286fb49a2737f4ee9e085c1ecf288d9f
DyNamiC <imsong1997@gmail.com>
2020-04-12 16:13:06 +0900

    Update Data.js

--- Data.js
+++ Data.js
@@ -9,11 +9,12 @@
 // }
 
 // getData();
+import {currentLat, currentLon} from "./ShowMap";
 
-// var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
-//     center : new kakao.maps.LatLng(36.2683, 127.6358), // 지도의 중심좌표 
-//     level : 3 // 지도의 확대 레벨 
-// });
+var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
+    center : new kakao.maps.LatLng(currentLat, currentLon), // 지도의 중심좌표 
+    level : 3 // 지도의 확대 레벨 
+});
 
 // // 마커 클러스터러를 생성합니다 
 const clusterer = new kakao.maps.MarkerClusterer({
