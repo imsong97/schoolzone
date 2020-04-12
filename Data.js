9c3c368f3a0c0deccd12d05c53601b7176113884
DyNamiC <imsong1997@gmail.com>
2020-04-12 16:10:31 +0900

    Update Data.js

--- Data.js
+++ Data.js
@@ -10,10 +10,10 @@
 
 // getData();
 
-var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
-    center : new kakao.maps.LatLng(36.2683, 127.6358), // 지도의 중심좌표 
-    level : 3 // 지도의 확대 레벨 
-});
+// var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
+//     center : new kakao.maps.LatLng(36.2683, 127.6358), // 지도의 중심좌표 
+//     level : 3 // 지도의 확대 레벨 
+// });
 
 // // 마커 클러스터러를 생성합니다 
 const clusterer = new kakao.maps.MarkerClusterer({
