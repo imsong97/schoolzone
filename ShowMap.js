0c05311061edeb7d73f9f286372b39d242ecf553
DyNamiC <imsong1997@gmail.com>
2020-04-12 21:03:05 +0900

    Update ShowMap.js

--- ShowMap.js
+++ ShowMap.js
@@ -14,8 +14,8 @@ function getMap(currentLat, currentLon){
 
     // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
     $.get("/data.json", function(data) { // 데이터에서 좌표 값을 가지고 마커를 표시합니다
-        var imageSrc = 'warning2.png', imageSize = new kakao.maps.Size(40, 40), // 마커이미지 크기
-        imageOption = {offset: new kakao.maps.Point(20, 40)}; 
+        var imageSrc = 'warning2.png', imageSize = new kakao.maps.Size(50, 50), // 마커이미지 크기
+        imageOption = {offset: new kakao.maps.Point(25, 50)}; 
         var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
         var markers = $(data.position).map(function(i, position) {
