c42ab409f9e594015ee28208fedd508b84a3329a
DyNamiC <imsong1997@gmail.com>
2020-04-12 16:45:27 +0900

    Update ShowMap.js

--- ShowMap.js
+++ ShowMap.js
@@ -2,13 +2,14 @@ const inputData = document.getElementById("adress"),
     btnSearch = document.getElementById("search");
 
 function getMap(currentLat, currentLon){
-    var container = document.getElementById('map'), // 지도를 표시할 div 
-    options = { 
-        center: new kakao.maps.LatLng(currentLat, currentLon), // 지도의 중심좌표
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
-    var map = new kakao.maps.Map(container, options);
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption); 
 
     //마커 표시
     var markerPosition  = new kakao.maps.LatLng(currentLat, currentLon); 
