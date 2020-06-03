1f1d47757239cb4f6159c55f370306e255ebd5ea
DyNamiC <imsong1997@gmail.com>
2020-06-03 15:18:56 +0900

    address search

--- ShowMap.js
+++ ShowMap.js
@@ -1,3 +1,6 @@
+const search = document.querySelector(".searchButton");
+const address = document.querySelector(".searchInput")
+
 function getMap(currentLat, currentLon){
     
     var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div 
@@ -43,16 +46,41 @@ function getMap(currentLat, currentLon){
     //지도 확대축소
     var zoomControl = new kakao.maps.ZoomControl();
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT); 
+
+    search.addEventListener("click", getAddress(map));
+}
+
+function getAddress(map){
+    // 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(address, function(result, status) {
+
+        // 정상적으로 검색이 완료됐으면 
+        if (status === kakao.maps.services.Status.OK) {
+
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
+
+            marker.setMap(map);
+
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+        } 
+    });    
 }
 
 function init(){
     navigator.geolocation.getCurrentPosition(function(position) { //현재위치
-        
         const currentLat = position.coords.latitude, // 위도
         currentLon = position.coords.longitude; // 경도
-        
         getMap(currentLat, currentLon);
-        
     });
 }
 
