71e68dd010665ab536a8df016b9e950700fddb70
DyNamiC <imsong1997@gmail.com>
2020-06-03 15:41:23 +0900

    Update ShowMap.js

--- ShowMap.js
+++ ShowMap.js
@@ -5,15 +5,21 @@ const address = document.querySelector(".searchInput");
 navigator.geolocation.getCurrentPosition(function(position) { 
     const currentLat = position.coords.latitude, // 위도
     currentLon = position.coords.longitude; // 경도
-    // getMap(currentLat, currentLon);
+    getMap(currentLat, currentLon);
 });
 
-var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div 
-    center : new kakao.maps.LatLng(currentLat, currentLon), // 지도의 중심좌표 
-    level : 3 // 지도의 확대 레벨 
-});
+function getMap(currentLat, currentLon){
+    var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div 
+        center : new kakao.maps.LatLng(currentLat, currentLon), // 지도의 중심좌표 
+        level : 3 // 지도의 확대 레벨 
+    });
 
-// // 마커 클러스터러를 생성합니다 
+    return map;
+}
+
+let map = getMap();
+
+// 마커 클러스터러를 생성합니다 
 const clusterer = new kakao.maps.MarkerClusterer({
     map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
     averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
@@ -54,16 +60,7 @@ map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 search.addEventListener("click", getAddress);
 
-// function getMap(currentLat, currentLon){
-    
-    
-// }
-
 function getAddress(){
-    var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div 
-        center : new kakao.maps.LatLng(currentLat, currentLon), // 지도의 중심좌표 
-        level : 3 // 지도의 확대 레벨 
-    });
 
     // 주소-좌표 변환 객체를 생성합니다
     var geocoder = new kakao.maps.services.Geocoder();
